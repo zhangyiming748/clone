97a239d071fbde9fcc5722926144d86a30012426
zen <mbp>
2025-04-13 23:09:10 +0800

    feat: 初始化项目，添加.gitignore、go.mod、README.md、CI工作流及主程序
    此次提交包含以下主要变更：
    - 添加.gitignore文件，忽略.DS_Store等系统文件
    - 初始化go.mod文件，定义模块名和Go版本
    - 添加README.md文件，描述项目功能和用法
    - 添加GitHub Actions工作流，配置GoReleaser自动发布
    - 实现主程序main.go，支持批量克隆GitHub仓库

---
+++ README.md
@@ -0,0 +1,22 @@
+# 0x01 
+
+自行登录GitHub的gh ~~我这边使用最稳妥的token方式~~
+
+# 0x02
+
+获取你自己的全部仓库信息
+
+```bash
+gh repo list --limit 150 | tee repo.list
+```
+
+# 0x03
+
+运行程序批量克隆仓库
+
+```bash
+go run main.go
+```
+
+或者如果没有或不想安装go sdk 可以使用对应你系统架构的[release]()
+
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	
+)
+
+// ReadLines 从指定文件中读取所有行，并返回一个字符串切片
+func ReadLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func main() {
+	filePath := "/Users/zen/Downloads/backup/repo.list"
+	lines, err := ReadLines(filePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	for _, line := range lines {
+		//fmt.Printf("Original: %v\n", line)
+		parts := strings.Fields(line)
+		if len(parts) > 0 {
+			url := parts[0] // 获取字符串的第一部分
+			// 获取URL中最后一个/分隔符之后的字符串作为name
+			urlParts := strings.Split(url, "/")
+			name := urlParts[len(urlParts)-1]
+			url = strings.Join([]string{"https://github.com/", url}, "")
+			fmt.Printf("URL: %v\n", url)
+			cmd := exec.Command("git", "clone", url, name)
+			fmt.Println(cmd.String())
+			if out,err:=cmd.CombinedOutput();err!=nil{
+				fmt.Println(err)
+			}else{
+				fmt.Println(string(out))
+			}
+			
+		}
+	}
+}
